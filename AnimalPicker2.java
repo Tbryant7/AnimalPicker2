0136b518e48908267c2d954a8ab0070411509a37
Tbryant7 <tannonbryant@gmail.com>
2024-02-28 17:27:51 -0500

    Dog ASCII

--- AnimalPicker2.java
+++ AnimalPicker2.java
@@ -15,6 +15,16 @@ public class AnimalPicker2 {
             System.out.println("Which animal would you like to see: a dog, a cat, or a fish?");
             System.out.print("Enter your choice: ");
             choice = scanner.nextLine().toLowerCase();
+            // Checking what the user selected
+            if (choice.equals("dog")) {
+                // ASCII art for dog
+                System.out.println("              __");
+                System.out.println(" (\\,--------'()'--o");
+                System.out.println("  (_    ___    /~\"");
+                System.out.println("   (_)_)  (_)_)");
+                
+                break;
+            }
          }
     }
 }
