141023bad61f58e976a2764ecbed69dc57bd08a6
Tbryant7 <tannonbryant@gmail.com>
2024-02-28 17:09:54 -0500

    Imported scanner and started code

--- AnimalPicker2.java
+++ AnimalPicker2.java
@@ -2,3 +2,12 @@
  * 2/28/24
  * Animal Picker
  */ 
+
+ import java.util.Scanner;
+
+public class AnimalPicker2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice;
+    }
+}
